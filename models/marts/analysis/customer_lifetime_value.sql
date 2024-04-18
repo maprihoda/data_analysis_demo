4bceb40caa63ff2167c4b5bf4756b0c2ec1ab1e0
maprihoda <marek.prihoda@gmail.com>
2024-04-18 18:51:01 +0200

    Removed unnecessary join from customer lifetime value

--- models/marts/analysis/customer_lifetime_value.sql
+++ models/marts/analysis/customer_lifetime_value.sql
@@ -1,13 +1,11 @@
 SELECT
     o.customer_id,
     COUNT(DISTINCT o.order_id) AS total_orders,
-    ROUND(SUM(d.quantity * d.unit_price * (1 - d.discount))) AS total_revenue,
-    ROUND(SUM(d.quantity * d.unit_price * (1 - d.discount)) / COUNT(DISTINCT o.order_id)) AS avg_order_value,
-    ROUND(SUM(d.quantity * d.unit_price * (1 - d.discount))) AS customer_lifetime_value
+    ROUND(SUM(o.quantity * o.unit_price * (1 - o.discount))) AS total_revenue,
+    ROUND(SUM(o.quantity * o.unit_price * (1 - o.discount)) / COUNT(DISTINCT o.order_id)) AS avg_order_value,
+    ROUND(SUM(o.quantity * o.unit_price * (1 - o.discount))) AS customer_lifetime_value
 FROM
-    {{ ref('int_order_details_orders') }} d
-INNER JOIN
-    {{ ref('stg_northwind_orders') }} o ON d.order_id = o.order_id
+    {{ ref('int_order_details_orders') }} o
 GROUP BY
     o.customer_id
 ORDER BY
