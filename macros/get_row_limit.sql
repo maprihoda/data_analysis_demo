282ccce76c3fb9f52147bf32981f25fa9c0246f6
maprihoda <marek.prihoda@gmail.com>
2024-04-19 18:27:53 +0200

    Simplify the get_row_limit macro

--- macros/get_row_limit.sql
+++ macros/get_row_limit.sql
@@ -1,12 +1,9 @@
 {% macro get_row_limit(custom_limit=None) %}
-    {% if target.name != 'prod' %}
-        {% if custom_limit %}
-            {% set row_limit = "LIMIT " ~ custom_limit %}
-        {% else %}
-            {% set row_limit = "LIMIT " ~ env_var('DBT_ROW_LIMIT') %}
-        {% endif %}
+    {% if custom_limit %}
+        {% set row_limit = "LIMIT " ~ custom_limit %}
     {% else %}
-        {% set row_limit = "" %}
+        {% set row_limit = "LIMIT " ~ env_var('DBT_ROW_LIMIT') %}
     {% endif %}
+
     {{ return(row_limit) }}
 {% endmacro %}
